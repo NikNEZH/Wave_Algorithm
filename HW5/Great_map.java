93095ff4fc0da574ef553315c88fced98185ded0
NIKITA <kit.zaver@yandex.ru>
2023-02-25 11:16:04 +0300

    Print and great map

--- HW5/Great_map.java
+++ HW5/Great_map.java
@@ -1,18 +1,12 @@
 package HW5;
 
 import java.util.Arrays;
-import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Great_map {
     public static void main(String[] args) {
-        int[][] arr = new int[10][10];
+        int[][] arr = new int[6][6];
         arrayCreat(arr);
-        // arrayFilling(arr);
-    //     while (arr.length >= 0) {
-    //         if()
-    //    }
-        
         // вывод построчно
         Arrays.stream(arr).map(Arrays::toString).forEach(System.out::println);
      
@@ -20,10 +14,16 @@ public class Great_map {
 
     public static void arrayCreat(int[][] arr) {
          // создание массива
-         IntStream.range(0, 10).forEach(i -> {
+         IntStream.range(0, 6).forEach(i -> {
             arr[i][i] = 0;
-            int a = (int) ( Math.random() * 3 );
-            arr[i][1 + a] = -1;
+            arr[5][i] = -1;
+            arr[0][i] = -1;
+            arr[i][0] = -1;
+            arr[i][5] = -1;
+        // ставим искуственно барьеры
+            arr[1][3] = -1;
+            arr[2][3] = -1;
+            arr[3][3] = -1;
         });
     }
 }
