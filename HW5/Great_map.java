40b10eadcb94afe639c4bbaa0a7153cda1a071cf
Royal00Blood <nazgoool1@gmail.com>
2023-02-25 22:49:29 +0400

    create new 5 methods

--- HW5/Great_map.java
+++ HW5/Great_map.java
@@ -1,10 +1,8 @@
 package HW5;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
-import javax.swing.RowFilter;
-
+// import java.util.Arrays;
+// import java.util.stream.IntStream;
+// import javax.swing.RowFilter;
 import java.util.Scanner;
 public class Great_map {
     public static void main(String[] args) {
@@ -15,18 +13,18 @@ public class Great_map {
         //начальные данные
         System.out.println("Введите количество выходов");
         int count = input.nextInt();
-        int [] pointStart = startPosition();
-        int [] [] pointFinish = new int [count]{count}; 
+        int [] pointStart = StartPosition();
+        int [] pointsFinish = PointsFinish(); 
         //
-        arrayCreat(arr, pointStart);
+        ArrayCreat(arr, pointStart,pointsFinish);
 
         // Маркировка маршрута
         PathMarking(arr, pointStart[0], pointStart[1]);
         // end
-        System.out.println(rawData(arr));
+        System.out.println(RawData(arr));
     }
 
-    static String rawData(int[][] map) {
+    static String RawData(int[][] map) {
         // init
         StringBuilder sb = new StringBuilder();
         // body
@@ -50,7 +48,7 @@ public class Great_map {
         return array;
     } 
     
-    private static int[] startPosition(){
+    private static int[] StartPosition(){
         // init
         Scanner input = new Scanner(System.in);
         int [] point = new int[2];
@@ -58,12 +56,18 @@ public class Great_map {
         System.out.print("Введите номер строки: ");
         point[0] = input.nextInt();
         System.out.print("Введите номер столбца: ");
-        point[0] = input.nextInt();
+        point[1] = input.nextInt();
         return point;
     }
 
-    public static void arrayCreat(int[][] arr,int[] start) {
-         // создание массива
+    public static void ArrayCreat(int[][] arr,int[] start, int []finish) {
+         // создание массива-карты
+         //////////////////////
+         // -1  -> стена
+         // -5  -> финиш
+         //  1  -> старт 
+         //////////////////////
+         // формируем стены 
          for (int i = 0; i < arr.length; i++) {
             arr[0][i] = -1;
             arr[arr.length-1][i] = -1;
@@ -72,9 +76,44 @@ public class Great_map {
             arr[i][0] = -1;
             arr[i][arr.length-1] = -1;
          }
-
+        // обозначаем старт 
         arr[start[0]][start[1]] = 1;
+        // наносим финишные точки
+        for (int i = 0; i < finish.length; i+=2) {
+            arr[i][i+1] = -5;
+        }
+        // обозначаем препядствия
+        //
+
+
     }
+    
+    private static int[] PointsFinish(){
+        // init
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите введите количествол выходов: ");
+        int countPoint = input.nextInt();
+
+        int [] points = new int[countPoint*2];
+        // body
+            // Записываем в массив по 2 элемента => шаг увеличиваем на 2
+            for (int i = 0; i < countPoint; i+=2) {
+                String str = String.format("Ввод точки финиша № ", i);
+                System.out.print(str);
+                System.out.print("Введите номер строки: ");
+                points[i] = input.nextInt();
+                System.out.print("Введите номер столбца: ");
+                points[i+1] = input.nextInt();
+            
+            }
+        // end
+        return points;
+    }
+    /**
+     * InnerGreat_map
+     */
+    
+
     public static void PathMarking(int[][] arr, int x, int y) {
         //[строка][столбец]
         //ограничение по массиву
