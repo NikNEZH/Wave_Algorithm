85b181415e0ddaf758591829c664a3ba7e7dcc82
Royal00Blood <nazgoool1@gmail.com>
2023-02-25 17:47:36 +0400

    Create function PathMarking

--- HW5/Great_map.java
+++ HW5/Great_map.java
@@ -6,13 +6,19 @@ import java.util.stream.IntStream;
 public class Great_map {
     public static void main(String[] args) {
         int[][] arr = new int[6][6];
-        arrayCreat(arr);
+        //под очередь
+        int [] pointStart = {1,1};
+        //
+        arrayCreat(arr, pointStart);
         // вывод построчно
         Arrays.stream(arr).map(Arrays::toString).forEach(System.out::println);
-     
+        //маркировка маршрута
+        PathMarking(arr, pointStart[0], pointStart[1]);
+
+        Arrays.stream(arr).map(Arrays::toString).forEach(System.out::println);
     }
 
-    public static void arrayCreat(int[][] arr) {
+    public static void arrayCreat(int[][] arr,int[] start) {
          // создание массива
          IntStream.range(0, 6).forEach(i -> {
             arr[i][i] = 0;
@@ -24,6 +30,33 @@ public class Great_map {
             arr[1][3] = -1;
             arr[2][3] = -1;
             arr[3][3] = -1;
+            arr[start[0]][start[1]] = 1;
         });
+    }
+    public static int [][] PathMarking(int[][] arr, int x, int y) {
+        //[строка][столбец]
+        //ограничение по массиву
+                if (arr[x-1][y]!= -1 && arr[x-1][y] == 0){
+                    arr[x-1][y] = arr[x][y] + 1;
+                    arr = PathMarking(arr, x - 1, y);
+                }
+                if (arr[x][y+1]!= -1 && arr[x][y+1] == 0){
+                    arr[x][y+1] = arr[x][y] + 1;
+                    arr = PathMarking(arr, x , y + 1);
+                }
+                if (arr[x+1][y]!= -1 && arr[x+1][y] == 0){
+                    arr[x+1][y] = arr[x][y] + 1;
+                    arr = PathMarking(arr, x + 1 , y);
+                }
+                if (arr[x][y-1]!= -1 && arr[x][y-1] == 0){
+                    arr[x][y-1] = arr[x][y] + 1;
+                    arr = PathMarking(arr, x , y - 1);
+                }
+                /////////////////////////////////////////////////////////
+                System.out.println("________________________________");
+                Arrays.stream(arr).map(Arrays::toString).forEach(System.out::println);
+                System.out.println("________________________________");
+                /////////////////////////////////////////////////////////
+                return arr;
     }
 }
